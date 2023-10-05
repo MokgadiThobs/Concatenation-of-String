0b4ff210077a057dfdbd125d4247719833ebb777
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-05 02:04:30 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        BtnShow = New Button()
+        SuspendLayout()
+        ' 
+        ' BtnShow
+        ' 
+        BtnShow.Location = New Point(240, 176)
+        BtnShow.Name = "BtnShow"
+        BtnShow.Size = New Size(146, 54)
+        BtnShow.TabIndex = 0
+        BtnShow.Text = "Show"
+        BtnShow.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(BtnShow)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents BtnShow As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub BtnShow_Click(sender As Object, e As EventArgs) Handles BtnShow.Click
+
+        Dim text1, text2, text3, text4 As String
+        text1 = "Visual"
+        text2 = "Basic"
+        text3 = "2022"
+        text4 = text1 + " " + text2 + " " + text3
+        MsgBox(text4)
+    End Sub
+End Class
